fbcad68ba307bc05e1cec56f41ba08b7386e53d0
Mark Anders <mark@markanders.net>
2016-11-18 14:15:24 -0700

    Added top level project file

---
+++ DateTools/DateTools.h
@@ -0,0 +1,19 @@
+//
+//  DateTools.h
+//  DateTools
+//
+//  Created by Mark Anders on 11/18/16.
+//  Copyright © 2016 Matthew York. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DateTools.
+FOUNDATION_EXPORT double DateToolsVersionNumber;
+
+//! Project version string for DateTools.
+FOUNDATION_EXPORT const unsigned char DateToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateTools/PublicHeader.h>
+
+
